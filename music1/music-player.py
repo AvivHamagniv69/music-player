082a86bf8acc9ebaa87084bbe88bb3586ea2b0b7
AvivHamagniv <51734410+AvivHamagniv69@users.noreply.github.com>
2022-04-22 01:06:31 +0300

    a major bug i need to fix before i can call this finished
    ok so i added multi processing so i could check if a song is finished and if it is then skip to the next song but for the life of me i cant find something that can cancel an active input

--- music1/music-player.py
+++ music1/music-player.py
@@ -3,42 +3,54 @@ import vlc
 import time
 import os
 import numpy
-
-stop_or_nah = None
-
+from multiprocessing import *
 
 list_of_files, counter = [], 0
 for file in os.listdir("."):
-    if file.endswith(".m4a") or file.endswith(".mp3"):
+    if file.endswith((".m4a", ".mp3", ".flac")):
         print(len(list_of_files), os.path.join(".", file))
         list_of_files.append(file)
 list_of_files_len = len(list_of_files)
 
 
+def music_check(check, song, inp):
+    while check == 1:
+        time.sleep(1)
+    else:
+        song.stop()
+        inp = False
+
+
 def play_music(song_number):
-    print ('play music')
     media_player = vlc.MediaPlayer(song_number)
     media_player.play()
-    time.sleep(1)
+    time.sleep(0.1)
     value = media_player.is_playing()
-    print(value)
-    music_command = input("do what you want now")
-    run_action_by_command(media_player, music_command)
+    music_command = None
+    playing_check = Process(target=music_check, args=(value, media_player, music_command))
+    playing_check.start()
+    music_command = input("do what you want now | ")
+    run_action_by_command(media_player, music_command, playing_check)
 
 
+stop_or_nah = None
 def play_directory(directory):
-    print ('pd')
     number_of_song_playing = 0
     for f in range(list_of_files_len):
-        print(stop_or_nah)
         if stop_or_nah == 1:
             return
-        print("youre in pd loop")
         play_music(directory[number_of_song_playing])
         number_of_song_playing = number_of_song_playing + 1
 
 
-def run_action_by_command(song, inp):
+def kill_process(song_2, process_check):
+    try:
+        song_2.stop()
+        process_check.kill()
+    except:
+        pass
+def run_action_by_command(song, inp, process):
+    global stop_or_nah
     try:
         play_music(list_of_files[int(inp)])
     except:
@@ -49,7 +61,7 @@ def run_action_by_command(song, inp):
         list of all commands:
         h = lists all commands
         e = exits the app
-        s = stops the music
+        s = stops the music 
         pd = plays your entire directory:
         r = plays the entire directory in random order.
         sk = skips the song and moves to the next song in line
@@ -59,46 +71,62 @@ def run_action_by_command(song, inp):
         ''')
 
     if inp == "pd":
+        stop_or_nah = 0
+        kill_process(song, process)
         play_directory(list_of_files)
 
     if inp == "e":
+        try:
+            process.kill()
+        except:
+            pass
         exit()
 
     if inp == "sk":
-        song.stop()
+        kill_process(song, process)
 
     if inp == "s":
-        song.stop()
-        global stop_or_nah
+        kill_process(song, process)
         stop_or_nah = 1
 
     if inp == "p":
-        song.pause()
-        continu = input("would you like to resume? y/n ")
-        if continu == "y":
-            song.play()
-        if continu == "n":
-            time.sleep(0)
+        try:
+            song.pause()
+            should_stop = input("would you like to resume? y/n | ")
+            if should_stop == "y":
+                song.play()
+                music_command = input("do what you want now | ")
+                run_action_by_command(song, music_command)
+            if should_stop == "n":
+                time.sleep(0)
+        except:
+            pass
 
     if inp == "r":
-        ask_automatic = input("make it pass to the next song automatically? y/n ")
+        kill_process(song, process)
+        ask_automatic = input("make it pass to the next song automatically? y/n | ")
         if ask_automatic == "y":
             random_list_of_files = numpy.array(list_of_files)
             numpy.random.shuffle(random_list_of_files)
             print("the playlist is:",
-random_list_of_files)
+                  random_list_of_files)
             play_directory(random_list_of_files)
 
         if ask_automatic == "n":
-            a = random.randint(0, list_of_files_len)
-            if a > 0:
-                a = a - 1
-            play_music(list_of_files[a])
+            random_music = random.randint(0, list_of_files_len)
+            if random_music > 0:
+                random_music = random_music - 1
+            play_music(list_of_files[random_music])
+
+    else:
+        pass
 
 
 while True:
     main_command = input('''
 list of all commands:
+choose a song number
+d = print the entire directory
 h = lists all commands
 e = exits the app
 s = stops the music
@@ -108,5 +136,6 @@ sk = skips the song and moves to the next song in line
 p = pauses the song and allows you to resume it later
 r = resumes the paused song
 
+enter your command here:
 ''')
-    run_action_by_command(song="pass", inp=main_command)
+    run_action_by_command(song="nothing", inp=main_command, process="none")
